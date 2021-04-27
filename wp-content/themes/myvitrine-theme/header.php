f9a755f3f187fd2a3a39a751cf863abb088984a8
=carpitron <=b.carpentier911@gmail.com>
2021-04-27 11:10:16 +0200

    Bug fixed in header

--- wp-content/themes/myvitrine-theme/header.php
+++ wp-content/themes/myvitrine-theme/header.php
@@ -70,13 +70,13 @@
 		</section>
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'myvitrine-theme' ); ?></button>
 			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-					'menu_class' => 'navClass',
-				)
-			);
+			// wp_nav_menu(
+			// 	array(
+			// 		'theme_location' => 'menu-1',
+			// 		'menu_id'        => 'primary-menu',
+			// 		'menu_class' => 'navClass',
+			// 	)
+			// );
 			?>
 
 			
