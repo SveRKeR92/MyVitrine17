4f9c8abdd2f9ffd897c07c1d143b571247e1375c
Tom Louveau <chewie.louveau@gmail.com>
2021-04-27 11:02:13 +0200

    add while posts loop

--- wp-content/themes/myvitrine-theme/page-vitrines.php
+++ wp-content/themes/myvitrine-theme/page-vitrines.php
@@ -119,6 +119,21 @@ get_header();
 
 	<section class="all-vitrines">
 		<h1 class="all-vitrines-title">Toutes les vitrines</h1>
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+		
 	</section>
 
 	</main><!-- #main -->
